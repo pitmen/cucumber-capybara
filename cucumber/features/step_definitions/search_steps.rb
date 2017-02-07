03a39876099e7a7cee6a792679565049b57baf6d
Rafael Peixoto <rapesil15@gmail.com>
2017-02-07 17:55:06 -0200

    melhorando expect

--- cucumber/features/step_definitions/search_steps.rb
+++ cucumber/features/step_definitions/search_steps.rb
@@ -4,9 +4,12 @@ end
 
 Quando(/^eu digitar uma pesquisa$/) do
   fill_in 'q', with: 'teste'
+  find('#lst-ib').native.send_keys(:return)
+  sleep 5
 end
 
 Então(/^o google me trará resultados$/) do
   # falta entender como fazer essa validação de uma maneira melhor
   expect(page).to have_content 'test'
+  expect(page).to have_css('.g')
 end
